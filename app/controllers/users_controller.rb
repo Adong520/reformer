80db9b7d9195bce89ffb48f60b20b275250e5e4b
Adong520 <A18758@hotmail.com>
2017-05-25 11:54:40 +1000

    new and edit form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+	def new
+		@newuser = User.new
+		@firstuser = User.first
+	end
+
+	def create
+	end
+
+	#def create
+	#	@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+	#	if @user.save
+	#		redirect_to new_user_path
+	#	else
+	#		render :new
+	#	end
+	#end
+
+	
+	def create
+		@user = User.new(user_params)
+
+		if @user.save
+			redirect_to new_user_path
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	private
+
+		def user_params
+			params.require(:user).permit(:username, :email, :password)
+		end
+
+	
+
+end
